a32d27b468da44e3aaafc607299d4f3ee0fa9df2
CarlosEduJJ <168452931+CarlosEduJJ@users.noreply.github.com>
2024-05-13 16:33:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let rank = ["ferro" , "bronze", "prata", "ouro", "platina", "ascendente", "imortal", "radiante"]
 let heroi = ("carlos o heroico")
 
-const nome = ("o heroi com o nome" + heroi)
-console.log(nome)
+const nome = ("o heroi com o nome " + heroi)
+const continuacao =("esta no nivel " + rank[2])
+console.log(nome + continuacao)
