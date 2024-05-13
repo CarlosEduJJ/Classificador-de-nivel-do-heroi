4c20f2ba1cafdfaebcc59ca8fd084301c71a7343
CarlosEduJJ <carneiro.kadusilva@gmail.com>
2024-05-13 16:25:58 -0300

    atualizacao

---
+++ index.js
@@ -0,0 +1,5 @@
+let rank = ["ferro" , "bronze", "prata", "ouro", "platina", "ascendente", "imortal", "radiante"]
+let heroi = ("carlos o heroico")
+
+const nome = ("o heroi com o nome" + heroi)
+console.log(nome)
